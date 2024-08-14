31097789300e42c0cd05bf08d2d7db28337ad695
Utkarsh Mehta <utkarshmehta93@gmail.com>
2024-08-14 00:48:41 -0500

    first commit

---
+++ Infrastructure/Config/ProductConfiguration.cs
@@ -0,0 +1,13 @@
+using System;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Config;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+    }
+}
